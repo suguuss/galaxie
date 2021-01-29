325d45dfd73206f595d5047726d463905e488dba
suguuss <25065770+suguuss@users.noreply.github.com>
2021-01-29 20:00:13 +0100

    backup commit

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 
 #define WIDTH 1000
 #define HEIGHT 600
-#define NB_STARS 200
+#define NB_STARS 100
 #define DEBUG_STAR 15
-#define TROU_NOIR 0
+#define TROU_NOIR NB_STARS / 2
 
-const unsigned long long int temps = 100000000;
+const unsigned long long int temps = 1000000000;
 
-// Prototype
+// Prototypes
 int CreateWindow(int width, int height, particle *p);
 
 
@@ -21,13 +21,14 @@ int main()
 	vec2 minVec = {.x = 0, .y = 0};
 	vec2 maxVec = {.x = WIDTH, .y = HEIGHT};
 
+	// Creates a list of stars
 	particle *p = particle_create_random_list(minVec, maxVec, 1, 10, NB_STARS);
 	
+	// Set a star to be the black hole
 	p[TROU_NOIR].m = 1000;
 	p[TROU_NOIR].r.x = WIDTH / 2;
 	p[TROU_NOIR].r.y = HEIGHT / 2;
 
-	printf("\n\n");
 	CreateWindow(WIDTH, HEIGHT, p);
 }
 
@@ -40,30 +41,38 @@ void render(struct gfx_context_t *context, particle *p)
 
 	for (int i = 0; i < NB_STARS; i++)
 	{
-		
-
-		if (DEBUG_STAR)
+		// if (i == DEBUG_STAR)
+		if (0)
 		{
-			// printf("-----------------\n");
-			// vec2_print(forces[i]);
+			printf("-----------------\n");
+			particle_print(p[i]);
+			printf("Resultant force :\t");
+			vec2_print(forces[i]);
 			forces[i].x = (forces[i].x / p[i].m);
 			forces[i].y = (forces[i].y / p[i].m);
-			// vec2_print(forces[i]);
+			printf("Acceleration:\t\t");
+			vec2_print(forces[i]);
 			forces[i].x *= temps;
 			forces[i].y *= temps;
-			// vec2_print(forces[i]);
+			printf("Accel + tps:\t\t");
+			vec2_print(forces[i]);
 			forces[i].y *= -1;
 			forces[i].x *= -1;
-			// vec2_print(forces[i]);
+			printf("inverted :\t\t");
+			vec2_print(forces[i]);
+			p[i].r = vec2_add(p[i].r, forces[i]);
+			printf("End particle : \n");
+			particle_print(p[i]);
 		}
 		else
 		{
-			forces[i].x = (forces[i].x / p[i].m) * temps;
-			forces[i].y = (forces[i].y / p[i].m) * temps;
+			forces[i].x = (forces[i].x / p[i].m) * temps/* * (-1)*/;
+			forces[i].y = (forces[i].y / p[i].m) * temps/* * (-1)*/;
+			p[i].r = vec2_add(p[i].r, forces[i]);
 		}
 		
 
-		p[i].r = vec2_add(p[i].r, forces[i]);
+		
 	}
 	
 
